546dd71c75438d7da5e263966d6dd8cc08d41ffc
lumakk <macedisonlozano@gmail.com>
2024-09-15 05:09:38 +0800

    introduction.c

---
+++ introduction.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // Imports the standard input output library
+
+int main(){ // main function
+
+    printf("Hello World!"); // prints output to terminal
+    return 0; // terminate program, returns exit status
+}
